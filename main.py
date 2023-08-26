62a903e3baf9de60e73297a76fa1781a7c1eb1ee
norbi <pocsainorbertjanos@gmail.com>
2023-08-27 01:05:36 +0200

    Added auto detection

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import tkinter as tk
 
 root = tk.Tk()
 root.geometry("500x500")
+
+
 def get_selected_index():
     selected_index = listbox.curselection()
     if selected_index:
@@ -16,21 +18,28 @@ def get_selected_index():
     else:
         return None
 
+
 def find_perint():
     image_found = False
+    checker = False
     list = ['per1.png', 'per16.png', 'per10.png']
-
     while not image_found:
-        pyautogui.hold('shift')
-        res = pyautogui.locateCenterOnScreen(image=list[get_selected_index()], confidence=0.9, minSearchTime=0.5)
-        if res is None:
-            mouse.click('left')
-        else:
-            image_found = True
-            print("Found it!")
+        if pyautogui.locateCenterOnScreen(image="bluechecker.png", confidence=0.9, minSearchTime=0.5) is not None:
+            checker = True
+        while checker:
+            pyautogui.hold('shift')
+            res = pyautogui.locateCenterOnScreen(image=list[get_selected_index()], confidence=0.9, minSearchTime=0.5)
+            if keyboard.is_pressed('Tab'):
+                break
+            if res is None:
+                mouse.click('left')
+                checker = False
+            else:
+                image_found = True
+                print("Found it!")
 
 #START BUTTON
-B = Button(root, text="START", command=lambda:[find_perint()],height = 5, width=20).place(x=10, y=10)
+B = Button(root, text="START", command=find_perint(),height = 5, width=20).place(x=10, y=10)
 
 #LISTBOX
 list_of_mods = ['Either per 16 and per 10 for wand', 'Per 16 for wand', 'Per 10 for wand']
@@ -39,14 +48,6 @@ listbox = tk.Listbox(root)
 for item in list_of_mods:
     listbox.insert(tk.END, item)
 listbox.pack()
-
-
-
-
-
-
-
-
 
 root.mainloop()
 
