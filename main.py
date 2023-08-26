28abb8192e7efaca94e97154c88615b57c47b4f7
norbi <pocsainorbertjanos@gmail.com>
2023-08-27 00:13:59 +0200

    ADD UI

--- main.py
+++ main.py
@@ -3,21 +3,55 @@ import mouseinfo
 import mouse
 import time
 import pyauto
+import keyboard
+from tkinter import *
+import tkinter as tk
 
-
-image_found = False
-res = pyautogui.locateOnScreen('image.png', 1)
-per10 = pyautogui.locateOnScreen('per10.png', 1)
-list = ['per1.png', 'image.png']
-
-while not image_found:
-    res = pyautogui.locateCenterOnScreen(image=list[0], confidence=0.9, minSearchTime=0.5)
-    if res is None:
-        mouse.click('left')
+root = tk.Tk()
+root.geometry("500x500")
+def get_selected_index():
+    selected_index = listbox.curselection()
+    if selected_index:
+        return selected_index[0]
     else:
-        image_found = True
+        return None
 
-print(res)
+def find_perint():
+    image_found = False
+    list = ['per1.png', 'per16.png', 'per10.png']
+
+    while not image_found:
+        pyautogui.hold('shift')
+        res = pyautogui.locateCenterOnScreen(image=list[get_selected_index()], confidence=0.9, minSearchTime=0.5)
+        if res is None:
+            mouse.click('left')
+        else:
+            image_found = True
+            print("Found it!")
+
+#START BUTTON
+B = Button(root, text="START", command=lambda:[find_perint()],height = 5, width=20).place(x=10, y=10)
+
+#LISTBOX
+list_of_mods = ['Either per 16 and per 10 for wand', 'Per 16 for wand', 'Per 10 for wand']
+
+listbox = tk.Listbox(root)
+for item in list_of_mods:
+    listbox.insert(tk.END, item)
+listbox.pack()
+
+
+
+
+
+
+
+
+
+root.mainloop()
+
+
+
 
 
 
